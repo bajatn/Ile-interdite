09ea6902a005fe0b19981fc8efa2328e17a5d798
bajatn <bajatn@pc-dg-033-01>
2017-06-19 10:54:34 +0200

    Coucou circonstences

--- ile-interdite/src/ile_interdite/Messager.java
+++ ile-interdite/src/ile_interdite/Messager.java
@@ -6,6 +6,7 @@
 package ile_interdite;
 
 import java.awt.Color;
+import java.util.ArrayList;
 
 /**
  *
@@ -15,5 +16,19 @@ public class Messager extends Aventurier {
 
     public Messager(Tuile tuile) {
         super("Messager", tuile, Color.GRAY);
+    }
+    
+    @Override
+    public ArrayList<Aventurier> donnerCarte(){
+        ArrayList<Aventurier> choixAventuriers = new ArrayList<Aventurier>();
+        ArrayList<Tuile> collecTuiles = getTuileActu().getGrille().getTuiles();
+        for (Tuile tuile: collecTuiles){
+            if (!(tuile.getAventuriers().isEmpty())){
+                for (Aventurier elem : tuile.getAventuriers()){
+                    choixAventuriers.add(elem); 
+                }
+            }
+        }
+        return choixAventuriers;   
     }
 }
